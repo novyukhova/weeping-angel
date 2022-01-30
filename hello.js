1cda4967301e69967091dd9f65faba8b731dacc0
novyukhova <naduff33@gmail.com>
2022-01-30 22:25:44 +0500

    create list of actions

--- hello.js
+++ hello.js
@@ -1,36 +1,55 @@
 let room = {
     wall: {
-        wallpaper: {
-            isRippedOff: false,
-        },
         lookAt: function () {
             if (!this.wallpaper.isRippedOff) {
-                console.log(`Осматривая стену, вы заметили, что край обоев плохо приклеен.
-Вы можете потянуть за край обоев или осмотреть комнату.`);
+                console.log(`Осматривая стену, вы заметили, что край обоев плохо приклеен.`);
                 this.seen = true;
             } else {
                 console.log(`На стене под оборванными обоями написано ваше имя.`);
             }
         },
-        seen: false
+        seen: false,
+        wallpaper: {
+            ripOff: function () {
+                console.log(`Потянув за краешек, вы оборвали обои. На стене написано "Берегись, ${name}!"`);
+                this.isRippedOff = true;
+            },
+            isRippedOff: false
+        }
+    },
+    lookAt: function () {
+        console.log(`В комнате всюду облезшие обои, паутина и пыль. Посередине упавшая старинная люстра, слева большое окно, а справа дверь. Куда же она ведет?`);
+        this.seen = true;
     },
     seen: false
 };
 
+function listAvailableActions() {
+    let actions = [];
+
+    actions.push('осмотреть комнату');
+    if (room.seen) {
+        actions.push('открыть дверь');
+        actions.push('выглянуть в окно');
+    }
+    actions.push('осмотреть стены');
+    if (room.wall.seen) {
+        actions.push('потянуть за край обоев');
+    }
+    console.log(`Вы можете ${actions.join(', ')}`);
+
+}
+
 let name = prompt('Введите ваше имя');
-console.log(`Вы находитесь в старом заброшенном особняке. Перед вам стена с оборванными обоями и, кажется, там что-то написано.
-Вы можете осмотреть стены или осмотреть комнату.`);
+console.log(`Вы находитесь в старом заброшенном особняке. Перед вам стена с оборванными обоями и, кажется, там что-то написано.`);
+listAvailableActions();
 function act(action) {
     if (action === 'Осмотреть стену') {
         room.wall.lookAt();
     } else if (action === 'Осмотреть комнату') {
-        console.log(`В комнате всюду облезшие обои, паутина и пыль. Посередине упавшая старинная люстра, слева большое окно, а справа дверь. Куда же она ведет?
-Вы можете осмотреть стены, выглянуть в окно или попробовать открыть дверь.`);
-        room.seen = true;
+        room.lookAt();
     } else if (room.wall.seen && action === 'Потянуть за край обоев') {
-        console.log(`Потянув за краешек, вы оборвали обои. На стене написано "Берегись, ${name}!"`);
-        room.wall.wallpaper.isRippedOff = true;
-    } else {
-        console.log(`Вы можете осмотреть стены или осмотреть комнату.`);
+        room.wall.wallpaper.ripOff();
     }
+    listAvailableActions();
 }
